c7f5e2450ae6b8f206b42f77a857bc28d9e8def9
yoxxa <theyoxxa@protonmail.com>
2026-02-28 03:19:06 +0800

    feat:`ucc-gen init` project template with slight modifications to fit `uv`

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+uv run ucc-gen build --source package
+uv run ucc-gen package --path output/ta_cisco_bcs_operational_insights/
---
+++ scripts/cleanup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf output ta_cisco_bcs_operational_insights*
